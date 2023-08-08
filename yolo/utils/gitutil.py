c4b1bf5b37c526ae56b85c6558997f6eebd4b204
zjykzj <wy163zhuj@163.com>
2023-08-08 16:54:54 +0800

    perf(gitutil.py): update check_git_status

--- yolo/utils/gitutil.py
+++ yolo/utils/gitutil.py
@@ -45,7 +45,8 @@ def git_describe(path=ROOT):  # path must be a directory
 
 @TryExcept()
 @WorkingDirectory(ROOT)
-def check_git_status(repo='ultralytics/yolov5', branch='master'):
+# def check_git_status(repo='ultralytics/yolov5', branch='master'):
+def check_git_status(repo='zjykzj/YOLOv5', branch='master'):
     # YOLOv5 status check, recommend 'git pull' if code is out of date
     url = f'https://github.com/{repo}'
     msg = f', for updates see {url}'
